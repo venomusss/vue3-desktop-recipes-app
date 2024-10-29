6883557a442eec140905cb0fe2ea1c298dd9958e
venomusss <vovamushii@gmail.com>
2024-10-29 22:54:11 +0200

    fix: fixed build issue

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior() {
     return { top: 0 };
   },
 });
